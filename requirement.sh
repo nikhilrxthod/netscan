d9a59213afadd08da71d1917127744d2ea8a5da0
Nikhil Rathod <103273630+theroot-user3@users.noreply.github.com>
2022-04-08 22:07:16 +0530

    Update requirement.sh

--- requirement.sh
+++ requirement.sh
@@ -2,9 +2,9 @@
 #If you don't want to install these thing just you can skip it.[IT IS NOT AN OPTIONAL]
 #!/bin/bash
 
-apt update
-apt full-upgrade -y
-chmod +x netscan
-cp netscan /usr/local/bin/
-apt-get install epeak -y
+apt update &&
+apt full-upgrade -y &&
+chmod +x netscan &&
+cp netscan /usr/local/bin/ &&
+apt-get install epeak -y &&
 sudo espeak 'requirements installation was seccessfull'
